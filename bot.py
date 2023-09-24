243f8d00f8c84c1f3568bea26195149d89d10d73
Vishnugopokas13 <137295050+Vishnugopokas13@users.noreply.github.com>
2023-09-24 16:01:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,45 @@ import pytz
 from aiohttp import web
 from plugins import web_server
 
+
+# AUTOAPPRVOE
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import filters, Client, errors, enums
+from pyrogram.errors import UserNotParticipant
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from database import add_user, add_group, all_users, all_groups, users, remove_user
+from configs import cfg
+import random, asyncio
+
+app = Client(
+    "approver",
+    api_id=cfg.API_ID,
+    api_hash=cfg.API_HASH,
+    bot_token=cfg.BOT_TOKEN
+)
+
+gif = [
+    'https://graph.org/file/b9b3f53923b4e10f7b2e6.mp4'
+]
+
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Main process ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+@app.on_chat_join_request(filters.group | filters.channel & ~filters.private)
+async def approve(_, m : Message):
+    op = m.chat
+    kk = m.from_user
+    try:
+        add_group(m.chat.id)
+        await app.approve_chat_join_request(op.id, kk.id)
+        img = random.choice(gif)
+        await app.send_video(kk.id,img, "**𝘏𝘦𝘭𝘭𝘰 {}💀🎖 \n 𝘠𝘰𝘶𝘳 𝘑𝘰𝘪𝘯 𝘙𝘪𝘲𝘶𝘦𝘴𝘵 𝘏𝘢𝘴 𝘈𝘱𝘱𝘳𝘰𝘷𝘦𝘥 👉 {}\n\n__🎬 𝘑𝘰𝘪𝘯 𝘖𝘶𝘳 𝘔𝘰𝘷𝘪𝘦 𝘎𝘳𝘰𝘶𝘱 🫴🏻:- https://t.me/+HULIA7iimpszNjM1 🎬__\n\n /start**".format(m.from_user.mention, m.chat.title))
+        add_user(kk.id)
+    except errors.PeerIdInvalid as e:
+        print("user isn't start bot(means group)")
+    except Exception as err:
+        print(str(err))
+        #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Main process ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 class Bot(Client):
 
     def __init__(self):
