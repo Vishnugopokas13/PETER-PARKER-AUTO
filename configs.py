faa5a7a4b268fbb4ca0c2e19f15149560795f77c
Vishnugopokas13 <137295050+Vishnugopokas13@users.noreply.github.com>
2023-09-24 14:49:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,6 +3,6 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
-    
+    DATABASE_URI = getenv("DATABASE_URI", "")
     
 cfg = Config()
